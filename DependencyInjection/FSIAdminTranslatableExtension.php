be157c9a083a063ec404f98a4e7fccc2ad9055ef
Artur Czocher <artur.czocher@gmail.com>
2014-06-16 15:24:55 +0200

    Add bundle configuration

--- DependencyInjection/FSIAdminTranslatableExtension.php
+++ DependencyInjection/FSIAdminTranslatableExtension.php
@@ -14,8 +14,24 @@ class FSIAdminTranslatableExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        $container->setParameter('fsi_admin_translatable.languages', $config['languages']);
+        $this->setTemplateParameters($container, $config['templates']);
+
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
         $loader->load('listeners.xml');
+    }
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @param array $config
+     */
+    protected function setTemplateParameters(ContainerBuilder $container, $config = array())
+    {
+        foreach ($config as $key => $value) {
+            $container->setParameter(sprintf('admin_translatable.templates.%s', $key), $value);
+        }
     }
 }
