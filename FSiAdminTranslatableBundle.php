835d2fb09ad85aa11c883c8241b6af3a0514e864
Artur Czocher <artur.czocher@gmail.com>
2014-06-18 14:41:35 +0200

    Add compiler pass

--- FSiAdminTranslatableBundle.php
+++ FSiAdminTranslatableBundle.php
@@ -2,11 +2,24 @@
 
 namespace FSi\Bundle\AdminTranslatableBundle;
 
+use FSi\Bundle\AdminTranslatableBundle\DependencyInjection\Compiler\TranslatableElementPass;
 use FSi\Bundle\AdminTranslatableBundle\DependencyInjection\FSIAdminTranslatableExtension;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FSiAdminTranslatableBundle extends Bundle
 {
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new TranslatableElementPass(), PassConfig::TYPE_BEFORE_REMOVING);
+    }
+
     /**
      * @return FSIAdminTranslatableExtension
      */
