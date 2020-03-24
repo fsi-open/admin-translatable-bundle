37ca9b9a5c331f748eed69dc5cded69813272135
Piotr Szymaszek <pszymaszek@gmail.com>
2020-03-24 15:51:24 +0100

    Refactored Translatable DataGrid column

--- DataGrid/Extension/ColumnType/Translatable.php
+++ DataGrid/Extension/ColumnType/Translatable.php
@@ -67,10 +67,10 @@ class Translatable extends ColumnAbstractTypeExtension
         $notTranslated = false;
 
         foreach ($column->getOption('field_mapping') as $propertyPath) {
-            if ($this->isPropertyPathTranslatable($view, $propertyPath)) {
+            if (true === $this->isPropertyPathTranslatable($view, $propertyPath)) {
                 $translatable = true;
 
-                if ($this->isPropertyPathTranslated($view, $propertyPath)) {
+                if (true === $this->isPropertyPathTranslated($view, $propertyPath)) {
                     $notTranslated = true;
                 }
             }
@@ -80,69 +80,29 @@ class Translatable extends ColumnAbstractTypeExtension
         $view->setAttribute('not_translated', $notTranslated);
     }
 
-    private function validatePropertyPath(string $propertyPath): ?PropertyPath
-    {
-        $propertyPath = new PropertyPath($propertyPath);
-        return $propertyPath->isProperty($propertyPath->getLength() - 1)
-            ? $propertyPath
-            : null
-        ;
-    }
-
     private function getMostNestedObject(
         CellViewInterface $view,
         PropertyPathInterface $propertyPath
     ) {
-        return $propertyPath->getLength() > 1
-            ? $this->propertyAccessor->getValue(
+        if (1 < $propertyPath->getLength()) {
+            return $this->propertyAccessor->getValue(
                 $view->getSource(),
                 (string) $propertyPath->getParent()
-            )
-            : $view->getSource()
-        ;
-
-    }
-
-    private function getMostNestedProperty(PropertyPathInterface $propertyPath): string
-    {
-        return $propertyPath->getElement($propertyPath->getLength() - 1);
-    }
-
-    private function getTranslatableMetadata(
-        CellViewInterface $view,
-        PropertyPathInterface $propertyPath
-    ): ClassMetadata {
-        $object = $this->getMostNestedObject($view, $propertyPath);
-
-        return $this->getObjectTranslatableMetadata($object);
-    }
-
-    private function getObjectTranslatableMetadata($object): ClassMetadataInterface
-    {
-        $class = get_class($object);
-        $manager = $this->managerRegistry->getManagerForClass($class);
-
-        return $this->translatableListener->getExtendedMetadata($manager, $class);
-    }
-
-    private function isPropertyPathTranslated(CellViewInterface $view, string $propertyPath): bool
-    {
-        $propertyPath = $this->validatePropertyPath($propertyPath);
-        if (!$propertyPath) {
-            return false;
+            );
         }
 
-        return $this->getObjectLocale($view, $propertyPath) !== $this->translatableListener->getLocale();
+        return $view->getSource();
     }
 
-    private function isPropertyPathTranslatable(CellViewInterface $view, string $propertyPath): bool
+    private function isPropertyPathTranslatable(CellViewInterface $view, string $stringPath): bool
     {
-        if (!($propertyPath = $this->validatePropertyPath($propertyPath))) {
+        $propertyPath = $this->transformPathToPropertPathObjectIfValid($stringPath);
+        if (null === $propertyPath) {
             return false;
         }
 
         $translatableMetadata = $this->getTranslatableMetadata($view, $propertyPath);
-        if (!$translatableMetadata->hasTranslatableProperties()) {
+        if (false === $translatableMetadata->hasTranslatableProperties()) {
             return false;
         }
 
@@ -156,11 +116,52 @@ class Translatable extends ColumnAbstractTypeExtension
         return false;
     }
 
+    private function isPropertyPathTranslated(CellViewInterface $view, string $stringPath): bool
+    {
+        $propertyPath = $this->transformPathToPropertPathObjectIfValid($stringPath);
+        if (null === $propertyPath) {
+            return false;
+        }
+
+        return $this->getObjectLocale($view, $propertyPath) !== $this->translatableListener->getLocale();
+    }
+
     private function getObjectLocale(CellViewInterface $view, $propertyPath): ?string
     {
         $object = $this->getMostNestedObject($view, $propertyPath);
         $translatableMetadata = $this->getObjectTranslatableMetadata($object);
 
         return $this->propertyAccessor->getValue($object, $translatableMetadata->localeProperty);
+    }
+
+    private function getTranslatableMetadata(
+        CellViewInterface $view,
+        PropertyPathInterface $propertyPath
+    ): ClassMetadata {
+        return $this->getObjectTranslatableMetadata(
+            $this->getMostNestedObject($view, $propertyPath)
+        );
+    }
+
+    private function getObjectTranslatableMetadata($object): ClassMetadataInterface
+    {
+        $class = get_class($object);
+        $manager = $this->managerRegistry->getManagerForClass($class);
+
+        return $this->translatableListener->getExtendedMetadata($manager, $class);
+    }
+
+    private function transformPathToPropertPathObjectIfValid(string $stringPath): ?PropertyPath
+    {
+        $propertyPath = new PropertyPath($stringPath);
+        return true === $propertyPath->isProperty($propertyPath->getLength() - 1)
+            ? $propertyPath
+            : null
+        ;
+    }
+
+    private function getMostNestedProperty(PropertyPathInterface $propertyPath): string
+    {
+        return $propertyPath->getElement($propertyPath->getLength() - 1);
     }
 }
